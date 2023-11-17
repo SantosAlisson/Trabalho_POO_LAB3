62f9cc40d42cf8ff16fba64a1bd8ff966ee4f3f8
ViniRomano <viniciusromanovpr@gmail.com>
2023-11-16 21:35:11 -0300

    new commit

--- estoque_automotivo/src/main/java/br/com/projeto/estoque_automotivo/controle/Controle.java
+++ estoque_automotivo/src/main/java/br/com/projeto/estoque_automotivo/controle/Controle.java
@@ -1,7 +1,22 @@
-   package br.com.projeto.estoque_automotivo.controle;
+package br.com.projeto.estoque_automotivo.controle;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crm.model.Veiculo;
+import com.example.crm.repository.VeiculoRepository;
 
 @RestController
    public class Controle {
